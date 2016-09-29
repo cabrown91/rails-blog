3650e94396513a8c123ec86de415e9d8b366fe41
cabrown91 <cleo.aliciabrown@gmail.com>
2016-09-29 06:28:29 -0700

    full crud blog working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   get "/" => "welcome#index"
+#   get "/about" => "welcome#about"
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "welcome#index"
-  get "/about" => "welcome#about"
+
+  resources :articles
+
+  root 'welcome#index'
 end
